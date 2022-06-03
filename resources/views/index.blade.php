15392275c9afc7cc68e0fa9a0da6109023faafe9
rancharaz <ranveer1990@hotmail.com>
2022-06-04 01:12:47 +0400

    pages-routing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.default')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body class="bg-slate-500">
-    ss
-</body>
-
-</html>
+@section('page-content')
+<div class="text-4xl text-white">
+    Home Page
+</div>
+@endsection
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ranveer Ancharaz</title>
+    <link rel="stylesheet" type="" href="{{ asset('css/app.css')}}">
+</head>
+
+<body class="bg-slate-500 text-white">
+    <header class="fixed bg-slate-600 top-0 left-0 right-0 z-50">
+        <div class="container mx-auto">
+            <h1 class="text-xl font-black">
+                Ranveer Ancharaz
+            </h1>
+            <nav class="-mx-2">
+                <a href="{{ route('home')}}"
+                    class="text-lg mx-2 text-white hover:text-indigo-400 transition-all">Home</a>
+                <a href=" {{ route('about')}}"
+                    class="text-lg mx-2 text-white hover:text-indigo-400 transition-all">About us</a>
+
+            </nav>
+        </div>
+    </header>
+    <main>
+        @yield('page-content')
+    </main>
+    <footer>
+        footer
+    </footer>
+</body>
+
+</html>
